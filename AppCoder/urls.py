5d6414944a615d3949fe5f3b935c1045566d9a8e
NicoV <nicolas.verdecchia@hotmail.com>
2023-02-26 23:22:49 -0300

    Templates Padre/Hijo + Nav Funcionando

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name="Inicio"), #esta era nuestra primera vista
     path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),    
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),    
 ]

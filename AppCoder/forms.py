c22f60bace4c54ded3b3b92768f9574d3dfac0b5
NicoV <nicolas.verdecchia@hotmail.com>
2023-02-27 01:42:13 -0300

    codigo funcionando - Formularios

---
+++ AppCoder/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class CursoFormulario(forms.Form):
+    #Especificar los campos 
+    curso = forms.CharField()
+    comision = forms.IntegerField()
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from AppCoder.models import Curso
+from AppCoder.models import *
+from AppCoder.forms import CursoFormulario
 
 # Create your views here.
 def inicio(request):
@@ -17,3 +18,27 @@ def entregables(request):
 
 def estudiantes(request):
     return render(request, 'AppCoder/estudiantes.html')
+'''
+def cursoFormulario(request):
+    if request.method == 'POST':
+        curso= Curso(request.POST['curso'], request.POST['comision'])
+        curso.save()
+        return render(request, "AppCoder/inicio.html")
+    
+    return render(request, "AppCoder/cursoFormulario.html")
+'''
+def cursoFormulario(request):
+    if request.method == 'POST':
+        miFormulario = CursoFormulario(request.POST)
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion['curso'], comision=informacion['comision'])
+            curso.save()
+            return render(request, "AppCoder/inicio.html")
+    
+    else:
+        miFormulario = CursoFormulario()
+
+    return render(request, "AppCoder/cursoFormulario.html", {"miFormulario":miFormulario})
